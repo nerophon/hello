6f2f42cf2767a4bbb32598970b0e6cd37f2ce78d
Nicholas Bellerophon <nerophon@gmail.com>
2015-05-21 16:27:23 +0100

    moar

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"github.com/nerophon/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
